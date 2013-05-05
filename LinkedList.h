21a33919c8e6c8673dfde24c17688a80387caac9
Matt <mcl209@vt.edu>
2013-05-05 19:32:12 -0400

    updated header file comments

--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,11 @@
+/* File Scrambler and Descrambler
+
+created by: James Dagres, Curtis Johnson, Matt Luckam, and Miguel Suarez
+
+This program scrambles a text file into a binary file, and displays the ouput.
+ It also can take the scrambled binary file, descramble it, and display the unscrambled file.
+*/
+
 #ifndef LinkedList_h
 #define LinkedList_h
 
